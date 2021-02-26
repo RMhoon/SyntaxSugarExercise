63cca0b22a6945a29168d70b8a746854760270a0
RMhoon <r_mhoon@hotmail.com>
2021-02-25 20:57:14 -0500

    Initialize project

---
+++ SyntaxSugarExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var anwser = 4;
+
+            var response = anwser < 9 ? $"{anwser} is less than nine." : $"{anwser} is greater than or equal to nine.";
+
+            Console.WriteLine(response);
+        }
+    }
+}
